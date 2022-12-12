d1dbb698a3d10b85811f66a27c63024f6c237006
Huypx3005 <Phamxuanhuy3005@gmail.com>
2022-12-12 20:49:42 +0700

    chat-app

---
+++ client/src/components/Chat/Chat.jsx
@@ -0,0 +1,77 @@
+import React, { useState, useEffect } from "react";
+import queryString from "query-string";
+import { useLocation } from "react-router-dom";
+import io from "socket.io-client";
+
+import TextContainer from '../TextContainer/TextContainer';
+import Messages from '../Messages/Messages';
+import InfoBar from '../InfoBar/InfoBar';
+import Input from '../Input/Input';
+
+import "./Chat.css";
+
+const ENDPOINT = "localhost:5000";
+
+let socket;
+
+const Chat = () => {
+  let location = useLocation();
+
+  const [name, setName] = useState("");
+  const [room, setRoom] = useState("");
+  const [users, setUsers] = useState("");
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    const { name, room } = queryString.parse(location.search);
+
+    socket = io(ENDPOINT, { transports: ["websocket"] });
+
+    setName(name);
+    setRoom(room);
+
+    socket.emit("join", { name, room }, () => {});
+
+    return () => {
+      socket.on("disconnect");
+
+      socket.off();
+    };
+  }, [location.search]);
+
+  useEffect(() => {
+    socket.on("message", (message) => {
+      setMessages((messages) => [...messages, message]);
+    });
+
+    socket.on("roomData", ({ users }) => {
+      setUsers(users);
+    });
+  }, []);
+
+  const sendMessage = (event) => {
+    event.preventDefault();
+
+    if(message) {
+      socket.emit('sendMessage', message, () => setMessage(''));
+    }
+  }
+  
+  return (
+    <div className="outerContainer">
+      <div className="container">
+        <InfoBar room={room} />
+        <Messages messages={messages} name={name} />
+        <Input
+          message={message}
+          setMessage={setMessage}
+          sendMessage={sendMessage}
+        />
+      </div>
+      <TextContainer users={users} />
+    </div>
+  );
+};
+
+export default Chat;
